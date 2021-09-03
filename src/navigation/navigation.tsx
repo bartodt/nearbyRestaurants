d2f05f32b2280fce51aecf8089e29d5464d89898
bartodt <bartolome@deltorto.com>
2021-09-03 17:47:34 -0300

    navigation + clean thrash

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,30 +1,59 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Header from '../components/Header/Header';
-import { Map } from '../components/Map';
+import CardList from '../components/restaurant-cards/CardList';
+import ReviewsList from '../components/reviews/ReviewsList';
+import Map from '../components/map/Map';
 import { Text } from 'react-native';
-import {ThemeContext} from 'styled-components/native';
+import { StackActions, useNavigation } from '@react-navigation/core';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+
 
 
 const Stack = createStackNavigator();
 
 const MainStack = () => {
+    const navigation = useNavigation()
     return (
         <Stack.Navigator
-            initialRouteName="Home"
-            screenOptions={{
-                header: () => (
-                    <Header />
-                ),
-            }}
+            initialRouteName="Mapa"
+            screenOptions={{ headerTitleAlign: 'center' }}
         >
             <Stack.Screen
                 name="Map"
                 component={Map}
                 options={{
-                    headerTitle: 'Home',
-                    headerRight: () => (
-                        <Text>Right</Text>
+                    title: "NearBite", headerRight: () => (
+                        <TouchableOpacity onPress={() => navigation.dispatch(StackActions.push("RestaurantList"))} style={{ marginLeft: 10 }}>
+                            <Text style={{ color: "#68D17E", marginRight: 10 }}>Details</Text>
+                        </TouchableOpacity>
+
+                    ),
+                }}
+            />
+            <Stack.Screen
+                name="RestaurantList"
+                component={CardList}
+                options={{
+                    title: "Near options", headerLeft: () => (
+                        <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 10 }}>
+                            <Text style={{ color: "#68D17E" }}>Back</Text>
+                        </TouchableOpacity>
+
+                    ),
+                }}
+
+            />
+            <Stack.Screen
+                name="Reviews"
+                component={ReviewsList}
+                options={{
+                    title: "Reviews", headerLeft: () => (
+                        <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 10 }}>
+                            <Text style={{ color: "#68D17E" }}>Back</Text>
+                        </TouchableOpacity>
+
                     ),
                 }}
 
