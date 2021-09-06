ec2469c539bd9226696332d659b35f5d1ffedfe4
bartodt <bartolome@deltorto.com>
2021-09-06 14:07:53 -0300

    Safe area provider

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import MainStack from './src/navigation/navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 const MyTheme = {
   ...DefaultTheme,
   colors: {
@@ -12,9 +13,11 @@ const MyTheme = {
 
 const App = () => {
   return (
+    <SafeAreaProvider>
     <NavigationContainer theme={MyTheme}>
         <MainStack />
     </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
